77cda59186550c2a8fead23e462ce56728938186
Bianca <bsineleck@gmail.com>
2024-12-22 23:36:04 -0300

    clear input search added

--- src/components/ProductsSearch.vue
+++ src/components/ProductsSearch.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="search-products"> 
-        <input type="text" id="search" name="search" placeholder="Pesquisar produtos" v-model="search" @keyup.enter.prevent="searchProducts"  />
-        <input class="btn primary hover search-btn" type="submit" @click.prevent="searchProducts" @keyup.enter.prevent="searchProducts" value="Pesquisar" />
+        <input type="text" id="search" name="search" placeholder="Pesquisar produtos" v-model="search" @keyup.enter.prevent="searchProducts"/>
+        <input class="btn primary hover search-btn" type="submit" @click.prevent="searchProducts" @keyup.enter.prevent="searchProducts" value=""/>
+        <button class="clear" @click="clearSearch" v-show="search"></button>
     </div>
 </template>
 
@@ -16,22 +17,51 @@ const searchProducts = () => {
     emit('emitSearchProducts', search.value);
 }
 
+const clearSearch = () => {
+    search.value = '';
+    emit('emitSearchProducts', search.value);
+}
 </script>
 
 <style lang="scss" scoped>
 .search-products {
     display: flex;
     justify-content: center;
-    margin-bottom: 40px;
     align-items: stretch;
+    position: relative;
+    width: fit-content;
+    margin: 0 auto 40px;
     input#search {
         border-color: transparent;
         margin-right: 10px;
-        width: 40%;
+        width: 600px;
         box-shadow: var(--shadow);
         &:focus {
             border-color: var(--primary-color);
         }
+    }
+    .search-btn {
+        width: 60px;
+        display: block;
+        background-image: url('../assets/img/icons/search.png');
+        background-size: 30px;
+        background-repeat: no-repeat;
+        background-position: center;
+        &:hover {
+            background-image: url('../assets/img/icons/search-hover.png');
+        }
+    }
+    .clear {
+        width: 10px;
+        height: 10px;
+        background-image: url('../assets/img/icons/clear.png');
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+        position: absolute;
+        right: 85px;
+        top: 50%;
+        transform: translateY(-50%);
     }
 }
 </style>
