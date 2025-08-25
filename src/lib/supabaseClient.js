4e5990b666b66cfd1ff829e7819369ff4b5ec84d
Bianca <bsineleck@gmail.com>
2025-08-25 15:42:26 -0300

    new env

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-	'https://tnwvrtitkleeayjechmy.supabase.co',
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRud3ZydGl0a2xlZWF5amVjaG15Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNDkwMjg5NCwiZXhwIjoyMDUwNDc4ODk0fQ.361_IfTGqxa5DhsoG4mBseSUasoAgCFj6CNTEEVg0xI'
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 )
